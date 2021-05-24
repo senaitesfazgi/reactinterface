1f7408360f0a829d7073d051759b0f9bab35bc5a
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2021-05-24 10:34:37 -0600

    Added Search Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { BiArchive } from "react-icons/bi"
+import Search from "./components/Search"
 function App() {
   return (
-    <div className="App">
-      <h1><BiArchive/>Your Appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl mb-3">
+        <BiArchive className="inline-block text-red-400 align-top"/>Your Appointments</h1>
+        <Search />
     </div>
   );
 }
